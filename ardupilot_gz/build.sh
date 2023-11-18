3725fe586c658534ecc2544edb29287a4592f3dc
hadongz <mhadifaturrahman@gmail.com>
2023-11-18 23:58:20 +0700

    update

--- ardupilot_gz/build.sh
+++ ardupilot_gz/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 GZ_VERSION=garden colcon build \
-   --cmake-clean-cache \
+  --cmake-clean-cache \
   --cmake-args \
     --no-warn-unused-cli \
     -DBUILD_TESTING=ON \
-    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
-    -DCMAKE_OSX_ARCHITECTURES="arm64" \
-    -DCMAKE_APPLE_SILICON_PROCESSOR="arm64" \
     -DCMAKE_PREFIX_PATH="~/ros2_ws/lib:$(brew --prefix):$(brew --prefix qt@5)" \
 
--- ardupilot_gz/setup.sh
+++ ardupilot_gz/setup.sh
@@ -5,10 +5,15 @@ if [[ ! -e $(pwd)/lib ]]; then
   mkdir lib 
 fi
 
-vcs import --input $curr_dir/extra_lib.repose lib
+vcs import --input $curr_dir/extra_lib.repos lib
 
-if [[ ! -e $(pwd)/src ]]
+if [[ ! -e $(pwd)/src ]]; then
   mkdir src
 fi
 
-vcs import --input https://raw.githubusercontent.com/ArduPilot/ardupilot_gz/main/ros2_gz_macos.repos src
+vcs import --input https://raw.githubusercontent.com/ArduPilot/ardupilot_gz/main/ros2_gz_macos.repos --recursive src
+
+project_dir=$(pwd)
+cd $project_dir/src/gps_umd/gpsd_client 
+touch COLCON_IGNORE
+cd $project_dir
